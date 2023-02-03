09b3a9dfc915fe328e9499f4c39769e2ee45c74d
Levi Wasmundt <leviwasmundt@gmail.com>
2023-02-02 18:10:38 -0800

    update connection

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("ecommerce_db", "root", "50ldpssbu50", {
+  : new Sequelize("DB", "USER", "PASSWORD", {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
